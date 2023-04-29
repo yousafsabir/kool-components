63353ee614223a1070d1f18ed4690f36ab68417d
yousafsabir <myousafdev@gmail.com>
2023-04-29 16:30:06 +0500

    ui: add DarkModeToggle & HamburgerMenu to components list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,24 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import DarkModeToggle from "@/components/darkModeToggle/DarkModeToggle";
+import HamburgerMenu from "@/components/hamburgerMenu/HamburgerMenu";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <h1 className='font-bold text-5xl'>Gonna be a site with Kool tsx components</h1>
-    </main>
-  )
+    const components = [<DarkModeToggle />, <HamburgerMenu />];
+    return (
+        <main
+            className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
+        >
+            <h1 className="font-bold text-5xl mb-5">
+                Gonna be a site with Kool tsx components
+            </h1>
+            <main className="grid grid-cols-2 max-w-3xl">
+                {components.map((component, i) => (
+                    <article className="py-4 px-2 w-[300px]" key={i}>{component}</article>
+                ))}
+            </main>
+        </main>
+    );
 }
