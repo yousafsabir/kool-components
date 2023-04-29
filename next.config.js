71c304abc49f7893d437db76396f25e6e8867a17
yousafsabir <myousafdev@gmail.com>
2023-04-29 16:16:34 +0500

    feat: disabled css modules in next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const path = require("path");
 
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    // disable css-modules component styling
+    webpack(config) {
+        config.module.rules.forEach((rule) => {
+            const { oneOf } = rule;
+            if (oneOf) {
+                oneOf.forEach((one) => {
+                    if (!`${one.issuer?.and}`.includes("_app")) return;
+                    one.issuer.and = [path.resolve(__dirname)];
+                });
+            }
+        });
+        return config;
+    },
+};
+
+module.exports = nextConfig;
